3d038027ec5fb444fc07889fa9c2f5416abffe3c
Markie Choi <markie054@gmail.com>
2022-02-14 15:59:50 +0900

    longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,18 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+
+  def new
+    @letters = ('a'..'z').to_a.sample(10).join(' ')
+  end
+
+  def score
+    @word = params[:word].strip.downcase.split('')
+    @letters = params[:letters].split
+    url = "https://wagon-dictionary.herokuapp.com/#{@word.join}"
+    word_serialized = URI.open(url).read
+    api_response = JSON.parse(word_serialized)
+    @is_valid = api_response['found']
+  end
+end
